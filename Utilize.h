ba585b727b08bd0ad2bfae5c133757e6cff37bf2
Nikoo Karimi <128680398+Nikooka@users.noreply.github.com>
2023-03-23 15:14:36 +0330

    Create Utilize.h

---
+++ Utilize.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <exception>
+#include <sstream>
+#include "Game.h"
+
+class Utilize{
+	public:
+		static bool command_interpreter(std::string , Game & );
+};		
